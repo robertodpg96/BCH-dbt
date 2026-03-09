ba6f51e70ff0f019cbf4e93ab7ad00913f4e894a
Roberto Palacios <roberto.palacios@student.ie.edu.com>
2026-03-09 22:57:21 +0100

    Formatting changes

--- bitcoin_cash/models/datamarts/mart_address_balances.sql
+++ bitcoin_cash/models/datamarts/mart_address_balances.sql
@@ -1,10 +1,4 @@
-{{
-    config(
-        materialized='table',
-        schema='datamart',
-        cluster_by=['address']
-    )
-}}
+{{ config(materialized="table", schema="datamart", cluster_by=["address"]) }}
 
 /*
     Data mart: current balance for all Bitcoin Cash addresses.
@@ -18,63 +12,59 @@
 
     Source: stg_bitcoin_cash__transactions (last 3 months of data)
 */
-
 with
 
--- Addresses that received outputs from a coinbase (block reward) transaction.
--- These are excluded from the final result per business requirements.
-coinbase_addresses as (
+    -- Addresses that received outputs from a coinbase (block reward) transaction.
+    -- These are excluded from the final result per business requirements.
+    coinbase_addresses as (
 
-    select distinct addr as address
-    from {{ ref('stg_bitcoin_cash__transactions') }}
-    cross join unnest(outputs) as output
-    cross join unnest(output.addresses) as addr
-    where is_coinbase = true
+        select distinct addr as address
+        from {{ ref("stg_bitcoin_cash__transactions") }}
+        cross join unnest(outputs) as output
+        cross join unnest(output.addresses) as addr
+        where is_coinbase = true
 
-),
+    ),
 
--- Total BCH received per address: sum of all output values directed to the address.
-received as (
+    -- Total BCH received per address: sum of all output values directed to the address.
+    received as (
 
-    select
-        addr                as address,
-        sum(output.value)   as total_received_satoshis
+        select addr as address, sum(output.value) as total_received_satoshis
 
-    from {{ ref('stg_bitcoin_cash__transactions') }}
-    cross join unnest(outputs) as output
-    cross join unnest(output.addresses) as addr
+        from {{ ref("stg_bitcoin_cash__transactions") }}
+        cross join unnest(outputs) as output
+        cross join unnest(output.addresses) as addr
 
-    group by addr
+        group by addr
 
-),
+    ),
 
--- Total BCH spent per address: sum of all input values originating from the address.
--- Coinbase inputs are excluded as they have no real sender address.
-spent as (
+    -- Total BCH spent per address: sum of all input values originating from the
+    -- address.
+    -- Coinbase inputs are excluded as they have no real sender address.
+    spent as (
 
-    select
-        addr                as address,
-        sum(input.value)    as total_spent_satoshis
+        select addr as address, sum(input.value) as total_spent_satoshis
 
-    from {{ ref('stg_bitcoin_cash__transactions') }}
-    cross join unnest(inputs) as input
-    cross join unnest(input.addresses) as addr
-    where is_coinbase = false
+        from {{ ref("stg_bitcoin_cash__transactions") }}
+        cross join unnest(inputs) as input
+        cross join unnest(input.addresses) as addr
+        where is_coinbase = false
 
-    group by addr
+        group by addr
 
-)
+    )
 
 select
     r.address,
     r.total_received_satoshis,
-    coalesce(s.total_spent_satoshis, 0)                                     as total_spent_satoshis,
-    r.total_received_satoshis - coalesce(s.total_spent_satoshis, 0)         as balance_satoshis,
-    (r.total_received_satoshis - coalesce(s.total_spent_satoshis, 0)) / 1e8 as balance_bch
+    coalesce(s.total_spent_satoshis, 0) as total_spent_satoshis,
+    r.total_received_satoshis - coalesce(s.total_spent_satoshis, 0) as balance_satoshis,
+    (r.total_received_satoshis - coalesce(s.total_spent_satoshis, 0))
+    / 1e8 as balance_bch
 
 from received r
-left join spent s
-    on r.address = s.address
+left join spent s on r.address = s.address
 
 -- Exclude addresses involved in any coinbase transaction
 where r.address not in (select address from coinbase_addresses)
--- bitcoin_cash/models/staging/stg_bitcoin_cash__transactions.sql
+++ bitcoin_cash/models/staging/stg_bitcoin_cash__transactions.sql
@@ -1,38 +1,32 @@
-{{
-    config(
-        materialized='view',
-        schema='staging'
-    )
-}}
+{{ config(materialized="view", schema="staging") }}
 
 /*
     Staging model for Bitcoin Cash transactions.
 
     Selects the last 3 months of transactions from the public BigQuery dataset.
-    This filter keeps queries within the BigQuery free tier on new GCP projects.
 
     Source: bigquery-public-data.crypto_bitcoin_cash.transactions
 */
-
 select
-    transactions.hash                   as transaction_hash,
+    transactions.hash as transaction_hash,
     transactions.block_hash,
     transactions.block_number,
     transactions.block_timestamp,
     transactions.block_timestamp_month,
-    transactions.size                   as transaction_size_bytes,
-    transactions.virtual_size           as virtual_size_bytes,
+    transactions.size as transaction_size_bytes,
+    transactions.virtual_size as virtual_size_bytes,
     transactions.version,
     transactions.lock_time,
     transactions.is_coinbase,
     transactions.input_count,
     transactions.output_count,
-    transactions.input_value            as input_value_satoshis,
-    transactions.output_value           as output_value_satoshis,
-    transactions.fee                    as fee_satoshis,
+    transactions.input_value as input_value_satoshis,
+    transactions.output_value as output_value_satoshis,
+    transactions.fee as fee_satoshis,
     transactions.inputs,
     transactions.outputs
 
-from {{ source('crypto_bitcoin_cash', 'transactions') }} as transactions
+from {{ source("crypto_bitcoin_cash", "transactions") }} as transactions
 
-where transactions.block_timestamp >= timestamp_sub(current_timestamp(), interval 90 day)
+where
+    transactions.block_timestamp >= timestamp_sub(current_timestamp(), interval 90 day)
